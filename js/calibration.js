6cfb307a03e8c86fa05db841fb1dffb39a9af596
PINKiiiiiiii <105914114+PINKiiiiiiii@users.noreply.github.com>
2023-02-05 22:37:49 +0700

    yeah

--- js/calibration.js
+++ js/calibration.js
@@ -92,7 +92,6 @@ $(document).ready(function () {
           store_points_variable(); // start storing the prediction points
 
           sleep(5000).then(() => {
-            webgazer.pause();
             stop_storing_points_variable(); // stop storing the prediction points
             var past50 = webgazer.getStoredPoints(); // retrieve the stored points
             var precision_measurement = calculatePrecision(past50);
